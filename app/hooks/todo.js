8837bad1e0f8a4ecb69db1471b2862f6eaf93c71
fsky <falcon.skydiving.dev@gmail.com>
2022-09-16 07:30:18 -0700

    add netlify cli

--- app/hooks/todo.js
+++ app/hooks/todo.js
@@ -142,7 +142,7 @@ export function useTodo() {
           )
     }
 
-
+ 
     const incompleteTodos = useMemo(() => todos.filter((todo) => !todo.account.marked), [todos])
     const completedTodos = useMemo(() => todos.filter((todo) => todo.account.marked), [todos])
 
